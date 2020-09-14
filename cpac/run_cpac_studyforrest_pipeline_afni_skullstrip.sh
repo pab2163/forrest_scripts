39b5793f48de3995de5846a27afb00717a55f70f
Paul Bloom <pab2163@holmes.cm.cluster>
2020-09-13 23:19:18 -0400

    run script for afni skullstrip pipeline test

---
+++ cpac/run_cpac_studyforrest_pipeline_afni_skullstrip.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --account=psych
+#SBATCH --job-name=forrest_cpac_fork_pipeine
+#SBATCH --mail-type=ALL
+#SBATCH --constraint=docker
+#SBATCH -c 8
+#SBATCH --time=11:55:00
+#SBATCH --mem-per-cpu=20gb
+
+sub=$1
+
+mkdir /rigel/psych/users/pab2163/studyforrest/preproc_forks/
+
+module load singularity
+singularity run \
+    -B /rigel/psych/users/pab2163/studyforrest/bids_raw:/bids_dataset \
+    -B /rigel/psych/users/pab2163/studyforrest/scripts/cpac/configs:/configs \
+    -B /rigel/psych/users/pab2163/studyforrest/preproc_forks:/outputs \
+    -B /rigel/psych/users/pab2163/studyforrest/scratch:/scratch \
+    /rigel/psych/users/pab2163/singularity_images/cpac_1_7_august_10_2020.simg /bids_dataset /outputs participant --participant_label $sub  --pipeline_file /configs/studyforrest_pipelines_afni_skullstrip.yaml --n_cpus 8 --mem_gb 20
