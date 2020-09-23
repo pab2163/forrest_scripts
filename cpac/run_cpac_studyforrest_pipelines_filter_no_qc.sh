0d522a9e901109a690d4dbefb1e32689f0ae1459
Paul A. Bloom <pab2163@columbia.edu>
2020-09-23 17:51:46 -0400

    testing 1.7.1 filtering

---
+++ cpac/run_cpac_studyforrest_pipelines_filter_no_qc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --account=psych
+#SBATCH --job-name=forrest_cpac_fork_pipeine_filter_no_qc
+#SBATCH --mail-type=ALL
+#SBATCH --constraint=docker
+#SBATCH -c 8
+#SBATCH --time=11:55:00
+#SBATCH --mem-per-cpu=20gb
+
+sub=$1
+
+mkdir /rigel/psych/users/pab2163/studyforrest/preproc_forks/
+
+module load singularity
+singularity run \
+    -B /rigel/psych/users/pab2163/studyforrest/bids_raw:/bids_dataset \
+    -B /rigel/psych/users/pab2163/studyforrest/scripts/cpac/configs:/configs \
+    -B /rigel/psych/users/pab2163/studyforrest/preproc_forks:/outputs \
+    -B /rigel/psych/users/pab2163/studyforrest/scratch:/scratch \
+    /rigel/psych/users/pab2163/singularity_images/cpac-1.71_dev.simg /bids_dataset /outputs participant --participant_label $sub  --pipeline_file /configs/studyforrest_pipelines_filter_no_qc.yaml --n_cpus 20 --mem_gb 20
